90368036ae5adf25841dc5853e5a7d85b32dd19d
jdelatorre312 <jdelatorre312@gmail.com>
2014-10-01 21:58:31 -0400

    Funky Unit Converter - Git
    Udemy Test

--- Funky Unit Converter/ViewController.m
+++ Funky Unit Converter/ViewController.m
@@ -18,6 +18,17 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    int x = 5, y=20, z=-2;
+    
+    int additionAnswer = x+y;
+    int multiplicationAnswer = y*z;
+    
+    float heightOfEverstBaseCamp = 16900.3;
+    float heightOfEverst = 29029;
+    
+    float distanceToTravel = heightOfEverst = heightOfEverstBaseCamp;
+    
+    NSLog(@"%i is addition and %i is multiplication \n and floats are %f", additionAnswer, multiplicationAnswer, distanceToTravel);
 }
 
 - (void)didReceiveMemoryWarning
